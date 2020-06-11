bd6498b854fd4b4537e7665162f363398b961b0f
caiquedbrandao <caique.dantas.brandao@gmail.com>
2020-06-11 00:47:33 -0300

    Merge request from  new branch.

--- src/client.py
+++ src/client.py
@@ -1,6 +1,9 @@
 import datetime
 from src import utils
 
+# just a text
+
+
 class Client:
 
     __slots__ = ['_name', '_last_name', '_cpf', '_birth_date']
@@ -9,7 +12,8 @@ class Client:
         self._name = name
         self._last_name = last_name
         self._cpf = cpf
-        self._birth_date = datetime.datetime.strptime(birth_date, '%d/%m/%Y').date()
+        self._birth_date = datetime.datetime.strptime(
+            birth_date, '%d/%m/%Y').date()
 
     def __str__(self):
         return "Nome: {} | CPF {}".format(self._name, self._cpf)
@@ -21,4 +25,3 @@ if __name__ == '__main__':
     print(cliente1)
     print(cliente1._birth_date)
     print(type(cliente1._birth_date))
-
